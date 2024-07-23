a6e604db83f05f2b389c238657f60d548cc76413
makjesus <makjesus2008@gmail.com>
2024-07-23 12:24:29 -0300

    First commit

---
+++ lasagna.rb
@@ -0,0 +1,26 @@
+# Define a constante com o tempo de cozimento por camada
+COOKING_TIME_PER_LAYER = 40
+
+# Função que retorna o tempo total de cozimento baseado no número de camadas
+def cooking_time_in_minutes(layers)
+  layers * COOKING_TIME_PER_LAYER
+end
+
+# Função que calcula o tempo restante com base no tempo de cozimento total e o tempo já passado
+def remaining_time_in_minutes(total_cooking_time, elapsed_time)
+  total_cooking_time - elapsed_time
+end
+
+# Função que retorna o tempo total de cozimento recomendado para a lasanha
+def total_cooking_time_in_minutes
+  80 # tempo total recomendado para a lasanha
+end
+
+# Exemplo de uso das funções
+layers = 3
+elapsed_time = 20
+
+total_cooking_time = total_cooking_time_in_minutes
+remaining_time = remaining_time_in_minutes(total_cooking_time, elapsed_time)
+puts "Tempo total de cozimento: #{cooking_time_in_minutes(layers)} minutos"
+puts "Tempo restante: #{remaining_time} minutos"
