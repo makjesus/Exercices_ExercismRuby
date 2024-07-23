91cdefd07ef477068df5030e48b3f71c50ffc1e4
makjesus <makjesus2008@gmail.com>
2024-07-23 13:10:49 -0300

    Third commit

--- lasagna.rb
+++ lasagna.rb
@@ -1,26 +1,23 @@
-# Define a constante com o tempo de cozimento
-COOKING_TIME_PER_LAYER = 15
+COOKING_TIME_PER_LAYER = 40
+RECOMMENDED_COOKING_TIME = 80
 
-# Função que retorna o tempo total de cozimento baseado no número de camadas
-def cooking_time_in_minutes(layers)
+# Função que calcula o tempo total de cozimento baseado no número de camadas
+def cooking_time(layers)
   layers * COOKING_TIME_PER_LAYER
 end
 
-# Função que calcula o tempo restante com base no tempo de cozimento total e o tempo já passado
-def remaining_time_in_minutes(total_cooking_time, elapsed_time)
-  total_cooking_time - elapsed_time
-end
-
-# Função que retorna o tempo total de cozimento recomendado para a lasanha
-def total_cooking_time_in_minutes
-  80 # tempo total recomendado para a lasanha
+# Função que calcula o tempo restante com base no número de camadas e o tempo já passado
+def remaining_time(layers, elapsed_time)
+  total_time = cooking_time(layers)
+  total_time - elapsed_time
 end
 
 # Exemplo de uso das funções
 layers = 3
 elapsed_time = 20
 
-total_cooking_time = total_cooking_time_in_minutes
-remaining_time = remaining_time_in_minutes(total_cooking_time, elapsed_time)
-puts "Esse foi o tempo total de cozimento: #{cooking_time_in_minutes(layers)} minutos"
-puts "Falta ainda: #{remaining_time} minutos"
+total_cooking_time = cooking_time(layers)
+remaining_time = remaining_time(layers, elapsed_time)
+
+puts "Tempo total de cozimento: #{total_cooking_time} minutos"
+puts "Tempo restante: #{remaining_time} minutos"
