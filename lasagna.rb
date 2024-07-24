c32a5184706dc547f4d35bf01f49b236e56488e6
makjesus <makjesus2008@gmail.com>
2024-07-24 10:48:25 -0300

    seventh commit

--- lasagna.rb
+++ lasagna.rb
@@ -1,3 +1,9 @@
+
+
+
+
+
+
 class Lasagna
   EXPECTED_MINUTES_IN_OVEN = 40
   MINUTES_PER_LAYER = 2
@@ -16,6 +22,35 @@ class Lasagna
 end
 
 lasagna = Lasagna.new
-puts lasagna.remaining_minutes_in_oven(30)
-puts lasagna.preparation_time_in_minutes(3) 
-puts lasagna.total_time_in_minutes(number_of_layers: 3, actual_minutes_in_oven: 30) # Espera-se 36
+puts "Faltam ainda no forno : #{lasagna.remaining_minutes_in_oven(30)}"
+puts "Tempo preparado em minutos :#{lasagna.preparation_time_in_minutes(3)}"
+puts "Tempo total gasto em minutos:#{lasagna.total_time_in_minutes(number_of_layers: 3, actual_minutes_in_oven: 30)}"
+
+
+=begin
+class Lasagna
+
+  #minutos de duração por padrão
+  MINUTOS_ESPERADOS_NO_FORNO  = 40
+  MINUTOS_POR_CAMADA = 2
+
+ #minutos que ainda restam no forno
+  def minutos_restantes_forno(minutos_atuais_forno)
+    MINUTOS_ESPERADOS_NO_FORNO  - minutos_atuais_forno
+  end
+
+   #tempo gasto na preparação de forma real
+  def tempo_de_preparacao_em_minutos(camadas)
+    MINUTOS_POR_CAMADA * camadas
+  end
+
+  #tempo total gasto na preparação
+  def tempo_total_em_minutos(numero_de_camadas:, minutos_atuais_forno:)
+    tempo_de_preparacao_em_minutos(numero_de_camadas) + minutos_atuais_forno
+  end
+end
+
+lasagna = Lasagna.new
+puts "Faltam ainda no forno : #{lasagna.remaining_minutes_in_oven(30)}"
+puts "Tempo preparado em minutos :#{lasagna.preparation_time_in_minutes(3)}"
+puts "Tempo total gasto em minutos:#{lasagna.total_time_in_minutes(number_of_layers: 3, actual_minutes_in_oven: 30)}"
