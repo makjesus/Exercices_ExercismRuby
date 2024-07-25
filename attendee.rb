92b10ed3254df81b7cd6073b478de21f6e0d4fea
makjesus <makjesus2008@gmail.com>
2024-07-25 10:46:42 -0300

    Third commit

--- attendee.rb
+++ attendee.rb
@@ -17,47 +17,47 @@ Alguns hóspedes quebram as regras com comportamentos inseguros, então o parque
 class Attendee
   attr_reader :height, :pass_id
 
+  #Implemente o método Attendee#initialize da classe Attendee,
+  #ele deve tomar uma altura (em centímetros) e armazená-la como
+  #uma variável de instância
   def initialize(height)
     @height = height
     @pass_id = nil
   end
 
+  #Implemente o getter Attendee#height
+  #da classe Attendee, ele deve retornar a altura das instâncias
+  #Nem todos os participantes compraram um passe de carona, mas precisamos saber
+  # se eles têm um passe ou não. Implemente o getter Attendee
+  #pass_id para a classe Attendee,ele deve retornar o pass_id da instância
+  #ou nil se o Attendee não tiver um.
   def issue_pass!(pass_id)
     @pass_id = pass_id
     @pass_id
   end
 
+
+  #Implemente o Attendee#issue_pass! para mutar o estado da instância e defina
+  #a variável de instância pass id para o argumento. Deve retornar o ID do passe.
   def revoke_pass!
     @pass_id = nil
   end
 end
 
+
 puts "Insira a altura do participante em centímetros:"
   height = gets.chomp.to_i
   attendee = Attendee.new(height)
 
-  puts "Altura do participante: #{attendee.height} cm"
-  puts "Pass ID antes da emissão: #{attendee.pass_id.inspect}"
+  puts "Digite a altura do participante: #{attendee.height} cm"
+  puts "Digite o  ID antes da emissão: #{attendee.pass_id.inspect}"
 
-  puts "Insira o ID do passe para emitir:"
+  puts "Digite o ID do passe para emitir:"
   pass_id = gets.chomp
   attendee.issue_pass!(pass_id)
 
-  puts "Pass ID após emissão: #{attendee.pass_id}"
+  puts "Esse é o Pass ID após emissão: #{attendee.pass_id}"
 
   attendee.revoke_pass!
-  puts "Pass ID após revogação: #{attendee.pass_id.inspect}"
+  puts "Esse é o Pass ID após revogação: #{attendee.pass_id.inspect}"
 
-=begin
-attendee = Attendee.new(150)
-puts attendee.height  # Output: 150
-puts attendee.pass_id # Output: nil
-
-# Issue a pass with ID 12345
-attendee.issue_pass!(12345)
-puts attendee.pass_id # Output: 12345
-
-# Revoke the pass
-attendee.revoke_pass!
-puts attendee.pass_id # Output: nil
-=end
