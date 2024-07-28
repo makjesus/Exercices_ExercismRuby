46e39e2899da85dff08c49e0eee303a986f1b660
makjesus <makjesus2008@gmail.com>
2024-07-28 14:09:07 -0300

    Second commit

--- AssemblyLine.rb
+++ AssemblyLine.rb
@@ -4,10 +4,20 @@ class AssemblyLine
   end
 
   def production_rate_per_hour
-    raise 'Please implement the AssemblyLine#production_rate_per_hour method'
+    #raise 'Please implement the AssemblyLine#production_rate_per_hour method'
+    @
+
+
   end
 
   def working_items_per_minute
-    raise 'Please implement the AssemblyLine#working_items_per_minute method'
+    #raise 'Please implement the AssemblyLine#working_items_per_minute method'
+    @
   end
 end
+
+AssemblyLine.new(6).production_rate_per_hour
+puts ""
+AssemblyLine.new(6).working_items_per_minute
+puts ""
+
